b9946b1cfe72f56a47ee6f131ef3ae846fa5e29b
Phil Edworthy <phil.edworthy@renesas.com>
2010-08-26 10:03:39 +0100

    tslib: Use more recent version - download will not work!
    The version of tslib on http://download.berlios.de/tslib is a little
    out of date. This patch makes BuildRoot use a new tarball created
    from http://github.com/kergoth/tslib. This tarball must be provided
    in the BR download directory.

--- package/tslib/tslib.mk
+++ package/tslib/tslib.mk
@@ -3,7 +3,7 @@
 # tslib
 #
 #############################################################
-TSLIB_VERSION:=1.0
+TSLIB_VERSION:=r77
 TSLIB_SOURCE:=tslib-$(TSLIB_VERSION).tar.bz2
 TSLIB_SITE:=http://download.berlios.de/tslib
 TSLIB_AUTORECONF = YES
