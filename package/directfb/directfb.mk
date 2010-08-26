24a310ff6b217920da57568cea9edfa48e07775a
Phil Edworthy <phil.edworthy@renesas.com>
2010-08-26 09:49:00 +0100

    DirectFB : Fix tslib detection

--- package/directfb/directfb.mk
+++ package/directfb/directfb.mk
@@ -20,6 +20,12 @@ define DIRECTFB_SH_TOOLCHAIN
 	$(SED) 's/sh4-\* | sh3-\*)/sh4-\* | sh3-\* | sh-\*)/' $(DIRECTFB_DIR)/configure.in
 endef
 DIRECTFB_POST_EXTRACT_HOOKS += DIRECTFB_SH_TOOLCHAIN
+
+# Fix tslib detection
+define DIRECTFB_TSLIB_VERSION
+	$(SED) 's/tslib-0.0/tslib-1.0/' $(DIRECTFB_DIR)/configure.in
+endef
+DIRECTFB_POST_EXTRACT_HOOKS += DIRECTFB_TSLIB_VERSION
 # Renesas End
 
 ifeq ($(BR2_PACKAGE_DIRECTFB_MULTI),y)
