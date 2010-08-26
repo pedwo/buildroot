9e5b56553953570fd438430eb6926f8958f828df
Phil Edworthy <phil.edworthy@renesas.com>
2010-08-26 09:47:38 +0100

    DirectFB: Add SH772x gfx driver

---
+++ package/directfb-sh772x-gfx/directfb-sh772x-gfx.mk
@@ -0,0 +1,74 @@
+#############################################################
+#
+# directfb_sh772x_gfx
+#
+#############################################################
+DIRECTFB_VERSION:=1.4.3
+DIRECTFB_SITE:=http://www.directfb.org/downloads/Core/DirectFB-$(DIRECTFB_VERSION_MAJOR)
+DIRECTFB_SOURCE:=DirectFB-$(DIRECTFB_VERSION).tar.gz
+DIRECTFB_SH772X_GFX_AUTORECONF = NO
+DIRECTFB_SH772X_GFX_INSTALL_STAGING = YES
+DIRECTFB_SH772X_GFX_INSTALL_TARGET = YES
+
+# BR2_LINUX26_VERSION is not really dependable
+# LINUX26_VERSION is not yet set.
+# Retrieve REAL kernel version from file.
+LINUX_FOR_DFB=`cat $(BUILD_DIR)/.linux-version`
+
+# We are operating in a specific dir of the DirectFB src
+DIRECTFB_DIR:=$(BUILD_DIR)/directfb-$(DIRECTFB_VERSION)
+DIRECTFB_SH772X_GFX_DIR:=$(DIRECTFB_DIR)/gfxdrivers/sh772x
+
+DIRECTFB_SH772X_GFX_MAKE_OPTS:=  KERNEL_VERSION=$(LINUX_FOR_DFB)
+DIRECTFB_SH772X_GFX_MAKE_OPTS += KERNEL_BUILD=$(BUILD_DIR)/linux-$(LINUX_FOR_DFB)
+DIRECTFB_SH772X_GFX_MAKE_OPTS += KERNEL_SOURCE=$(BUILD_DIR)/linux-$(LINUX_FOR_DFB)
+
+DIRECTFB_SH772X_GFX_MAKE_OPTS += SYSROOT=$(STAGING_DIR)
+#DIRECTFB_SH772X_GFX_MAKE_OPTS += ARCH=$(BR2_ARCH)
+DIRECTFB_SH772X_GFX_MAKE_OPTS += ARCH=sh
+DIRECTFB_SH772X_GFX_MAKE_OPTS += CROSS_COMPILE=$(TARGET_CROSS)
+DIRECTFB_SH772X_GFX_MAKE_OPTS += KERNEL_MODLIB=/lib/modules/$(LINUX_FOR_DFB)
+DIRECTFB_SH772X_GFX_MAKE_OPTS += DESTDIR=$(TARGET_DIR)
+DIRECTFB_SH772X_GFX_MAKE_OPTS += HEADERDIR=$(STAGING_DIR)
+
+
+$(DL_DIR)/$(DIRECTFB_SOURCE):
+	$(call DOWNLOAD,$(DIRECTFB_SITE),$(DIRECTFB_SOURCE))
+
+$(DIRECTFB_DIR)/.unpacked: $(DL_DIR)/$(DIRECTFB_SOURCE)
+	$(ZCAT) $(DL_DIR)/$(DIRECTFB_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
+	touch $@
+
+$(DIRECTFB_SH772X_GFX_DIR)/.install: $(DIRECTFB_DIR)/.unpacked
+	mkdir -p $(STAGING_DIR)/lib/modules/$(LINUX_FOR_DFB)/source/include/linux
+	$(MAKE) -C $(DIRECTFB_SH772X_GFX_DIR) -f Makefile.kernel \
+		$(DIRECTFB_SH772X_GFX_MAKE_OPTS)
+	$(MAKE) -C $(DIRECTFB_SH772X_GFX_DIR) -f Makefile.kernel install \
+		$(DIRECTFB_SH772X_GFX_MAKE_OPTS)
+	cp -dpf $(DIRECTFB_SH772X_GFX_DIR)/directfbrc.sh7723 $(TARGET_DIR)/etc/directfbrc
+	touch $@
+
+
+directfb_sh772x_gfx-source: $(DL_DIR)/$(DIRECTFB_SOURCE)
+
+directfb_sh772x_gfx-unpacked: $(DIRECTFB_DIR)/.unpacked
+
+directfb_sh772x_gfx: linux26 $(DIRECTFB_SH772X_GFX_DIR)/.install
+
+directfb_sh772x_gfx-clean:
+	-$(MAKE) -C $(DIRECTFB_SH772X_GFX_DIR) -f Makefile.kernel clean
+	rm -f $(TARGET_DIR)/lib/modules/$(LINUX_FOR_DFB)/renesas/sh772x_gfx.ko
+	rm -f $(DIRECTFB_SH772X_GFX_DIR)/.install
+
+directfb_sh772x_gfx-dirclean:
+	rm -rf $(DIRECTFB_DIR)
+
+#############################################################
+#
+# Toplevel Makefile options
+#
+#############################################################
+ifeq ($(BR2_PACKAGE_DIRECTFB_SH772X_GFX),y)
+TARGETS+=directfb_sh772x_gfx
+endif
+
--- package/directfb/directfb.mk
+++ package/directfb/directfb.mk
@@ -12,6 +12,16 @@ DIRECTFB_LIBTOOL_PATCH = NO
 DIRECTFB_INSTALL_STAGING = YES
 DIRECTFB_INSTALL_TARGET = YES
 
+# Renesas Start
+DIRECTFB_DIR:=$(BUILD_DIR)/directfb-$(DIRECTFB_VERSION)
+
+# Fix SH detection for CodeSourcery toolchain
+define DIRECTFB_SH_TOOLCHAIN
+	$(SED) 's/sh4-\* | sh3-\*)/sh4-\* | sh3-\* | sh-\*)/' $(DIRECTFB_DIR)/configure.in
+endef
+DIRECTFB_POST_EXTRACT_HOOKS += DIRECTFB_SH_TOOLCHAIN
+# Renesas End
+
 ifeq ($(BR2_PACKAGE_DIRECTFB_MULTI),y)
 DIRECTFB_MULTI:=--enable-multi --enable-fusion
 DIRECTFB_FUSION:=linux-fusion
@@ -65,6 +75,9 @@ DIRECTFB_GFX+= unichrome
 endif
 ifeq ($(BR2_PACKAGE_DIRECTFB_I830),y)
 DIRECTFB_GFX+= i830
+endif
+ifeq ($(BR2_PACKAGE_DIRECTFB_SH772X),y)
+DIRECTFB_GFX+= sh772x
 endif
 ifeq ($(DIRECTFB_GFX),)
 DIRECTFB_GFX:=none
