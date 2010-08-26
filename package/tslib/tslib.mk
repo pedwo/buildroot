8fa1fc1610e44fe27c82ff81d53dcb6e80dec1d4
Phil Edworthy <phil.edworthy@renesas.com>
2010-08-26 10:04:52 +0100

    tslib: Install 2 files to get tslib running on the MS7724 board out of the box.

--- package/tslib/tslib.mk
+++ package/tslib/tslib.mk
@@ -12,6 +12,16 @@ TSLIB_INSTALL_STAGING = YES
 TSLIB_INSTALL_TARGET = YES
 TSLIB_INSTALL_STAGING_OPT = DESTDIR=$(STAGING_DIR) LDFLAGS=-L$(STAGING_DIR)/usr/lib install
 
+# Other files
+define TSLIB_FILES
+	$(INSTALL) -m 0644 -D package/tslib/touch.env $(TARGET_DIR)
+	$(INSTALL) -m 0644 -D package/tslib/pointercal $(TARGET_DIR)/etc
+
+	$(SED) 's/\# module_raw input/module_raw input/' $(BUILD_DIR)/tslib-$(TSLIB_VERSION)/etc/ts.conf
+endef
+
+TSLIB_POST_EXTRACT_HOOKS += TSLIB_FILES
+
 TSLIB_CONF_OPT = \
 	--enable-shared	\
 	--prefix=/usr	\
