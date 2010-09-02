8cac7f6dc569036f5bc5f94e28ebd5fb9d92719a
Phil Edworthy <phil.edworthy@renesas.com>
2010-09-02 09:36:46 +0100

    Renesas: Bump uiomux & add download site

--- package/uiomux/uiomux.mk
+++ package/uiomux/uiomux.mk
@@ -3,9 +3,9 @@
 # uiomux
 #
 #############################################################
-UIOMUX_VERSION = 1.2.0
+UIOMUX_VERSION = 1.3.0
 UIOMUX_SOURCE = libuiomux-$(UIOMUX_VERSION).tar.gz
-UIOMUX_SITE = 
+UIOMUX_SITE = http://github.com/downloads/pedwo/libuiomux
 
 UIOMUX_AUTORECONF = YES
 UIOMUX_INSTALL_STAGING = YES
