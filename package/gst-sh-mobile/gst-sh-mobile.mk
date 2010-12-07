bc952e336af22ac5858ade9abec0c40b176f5139
Phil Edworthy <phil.edworthy@renesas.com>
2010-12-07 13:47:41 +0000

    Renesas: Bump SH packages

--- package/gst-sh-mobile/gst-sh-mobile.mk
+++ package/gst-sh-mobile/gst-sh-mobile.mk
@@ -3,7 +3,7 @@
 # gst-sh-mobile
 #
 #############################################################
-GST_SH_MOBILE_VERSION = 0.10.1
+GST_SH_MOBILE_VERSION = 0.10.2
 GST_SH_MOBILE_SOURCE = gst-sh-mobile-$(GST_SH_MOBILE_VERSION).tar.gz
 GST_SH_MOBILE_SITE = http://github.com/downloads/pedwo/gst-sh-mobile
 
@@ -11,6 +11,6 @@ GST_SH_MOBILE_AUTORECONF = YES
 GST_SH_MOBILE_INSTALL_STAGING = YES
 GST_SH_MOBILE_INSTALL_TARGET = YES
 
-GST_SH_MOBILE_DEPENDENCIES = gstreamer gst-plugins-base uiomux shcodecs shveu
+GST_SH_MOBILE_DEPENDENCIES = gstreamer gst-plugins-base uiomux shcodecs shveu shbeu
 
 $(eval $(call AUTOTARGETS,package,gst-sh-mobile))
--- package/shcodecs/shcodecs.mk
+++ package/shcodecs/shcodecs.mk
@@ -3,7 +3,7 @@
 # shcodecs
 #
 #############################################################
-SHCODECS_VERSION = 1.3.1
+SHCODECS_VERSION = 1.4.0
 SHCODECS_SOURCE = libshcodecs-$(SHCODECS_VERSION).tar.gz
 SHCODECS_SITE = http://github.com/downloads/pedwo/libshcodecs
 
