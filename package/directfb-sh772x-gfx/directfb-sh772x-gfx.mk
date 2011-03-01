c4b4be89cd429d2066a72c7c78d224e7b1596a45
Phil Edworthy <phil.edworthy@renesas.com>
2011-03-01 15:05:17 +0000

    Renesas: DirectFB: Removed unused bits of sh772x gfx package
    This fixes the issue of two DirectFB src dirs

--- package/directfb-sh772x-gfx/directfb-sh772x-gfx.mk
+++ package/directfb-sh772x-gfx/directfb-sh772x-gfx.mk
@@ -3,9 +3,8 @@
 # directfb_sh772x_gfx
 #
 #############################################################
-DIRECTFB_VERSION:=1.4.10
-DIRECTFB_SITE:=http://www.directfb.org/downloads/Core/DirectFB-$(DIRECTFB_VERSION_MAJOR)
-DIRECTFB_SOURCE:=DirectFB-$(DIRECTFB_VERSION).tar.gz
+DIRECTFB_VERSION_MAJOR:=1.4
+DIRECTFB_VERSION:=$(DIRECTFB_VERSION_MAJOR).10
 DIRECTFB_SH772X_GFX_AUTORECONF = NO
 DIRECTFB_SH772X_GFX_INSTALL_STAGING = YES
 DIRECTFB_SH772X_GFX_INSTALL_TARGET = YES
@@ -13,8 +12,7 @@ DIRECTFB_SH772X_GFX_INSTALL_TARGET = YES
 LINUX_FOR_DFB=$(BR2_LINUX_KERNEL_VERSION)
 
 # We are operating in a specific dir of the DirectFB src
-DIRECTFB_DIR:=$(BUILD_DIR)/directfb-$(DIRECTFB_VERSION)
-DIRECTFB_SH772X_GFX_DIR:=$(DIRECTFB_DIR)/gfxdrivers/sh772x
+DIRECTFB_SH772X_GFX_DIR:=$(BUILD_DIR)/directfb-$(DIRECTFB_VERSION)/gfxdrivers/sh772x
 
 DIRECTFB_SH772X_GFX_MAKE_OPTS:=  KERNEL_VERSION=$(LINUX_FOR_DFB)
 DIRECTFB_SH772X_GFX_MAKE_OPTS += KERNEL_BUILD=$(BUILD_DIR)/linux-$(LINUX_FOR_DFB)
@@ -29,14 +27,7 @@ DIRECTFB_SH772X_GFX_MAKE_OPTS += DESTDIR=$(TARGET_DIR)
 DIRECTFB_SH772X_GFX_MAKE_OPTS += HEADERDIR=$(STAGING_DIR)
 
 
-$(DL_DIR)/$(DIRECTFB_SOURCE):
-	$(call DOWNLOAD,$(DIRECTFB_SITE),$(DIRECTFB_SOURCE))
-
-$(DIRECTFB_DIR)/.unpacked: $(DL_DIR)/$(DIRECTFB_SOURCE)
-	$(ZCAT) $(DL_DIR)/$(DIRECTFB_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	touch $@
-
-$(DIRECTFB_SH772X_GFX_DIR)/.install: $(DIRECTFB_DIR)/.unpacked
+$(DIRECTFB_SH772X_GFX_DIR)/.install:
 	mkdir -p $(STAGING_DIR)/lib/modules/$(LINUX_FOR_DFB)/source/include/linux
 	$(MAKE) -C $(DIRECTFB_SH772X_GFX_DIR) -f Makefile.kernel \
 		$(DIRECTFB_SH772X_GFX_MAKE_OPTS)
@@ -46,9 +37,9 @@ $(DIRECTFB_SH772X_GFX_DIR)/.install: $(DIRECTFB_DIR)/.unpacked
 	touch $@
 
 
-directfb_sh772x_gfx-source: $(DL_DIR)/$(DIRECTFB_SOURCE)
+directfb_sh772x_gfx-source:
 
-directfb_sh772x_gfx-unpacked: $(DIRECTFB_DIR)/.unpacked
+directfb_sh772x_gfx-unpacked:
 
 directfb_sh772x_gfx: linux26 $(DIRECTFB_SH772X_GFX_DIR)/.install
 
@@ -58,7 +49,6 @@ directfb_sh772x_gfx-clean:
 	rm -f $(DIRECTFB_SH772X_GFX_DIR)/.install
 
 directfb_sh772x_gfx-dirclean:
-	rm -rf $(DIRECTFB_DIR)
 
 #############################################################
 #
