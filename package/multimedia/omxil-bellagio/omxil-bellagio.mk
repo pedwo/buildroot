93eafeb8def49dc3151322cd96af4c5c746aeabb
Phil Edworthy <phil.edworthy@renesas.com>
2011-04-19 15:28:37 +0100

    bellagio-omxil: Fix build dependency bug

--- package/multimedia/omxil-bellagio/omxil-bellagio.mk
+++ package/multimedia/omxil-bellagio/omxil-bellagio.mk
@@ -6,7 +6,6 @@
 OMXIL_BELLAGIO_VERSION = 0.9.2.1
 OMXIL_BELLAGIO_SOURCE = libomxil-bellagio-$(OMXIL_BELLAGIO_VERSION).tar.gz
 OMXIL_BELLAGIO_SITE = http://downloads.sourceforge.net/omxil/
-OMXIL_BELLAGIO_INSTALL_STAGING = YES
-OMXIL_BELLAGIO_LIBTOOL_PATCH = NO
+OMXIL_BELLAGIO_AUTORECONF = YES
 
 $(eval $(call AUTOTARGETS,package/multimedia,omxil-bellagio))
